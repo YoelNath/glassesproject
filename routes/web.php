8a7548b41f232bcce77884514c26fdcdfc289b0d
Andrew Wijaya <33770251+andrewwijaya96@users.noreply.github.com>
2023-12-07 21:32:08 +0700

    Lelah

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Auth::routes();
+Route::get('/lmao', function () {
+    return view('auth/login');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
