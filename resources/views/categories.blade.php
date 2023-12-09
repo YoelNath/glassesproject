3da83031dd49e4e30677408081bbaf8c906b7459
cent134rn <vincenttionando003@gmail.com>
2023-12-09 13:38:59 +0700

    Categoriess

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,35 @@
+{{-- @extends('components.navbar')
+@section('content') --}}
+
+<link rel="stylesheet" href="./Glassespedia/categories.css">
+
+<section class="product-categories">
+
+    <div class="products">
+
+        <div class="row"> <!-- 1. Men Glasses -->
+            <img src="./images" alt="">
+            <div class="con-text">
+                <h2>Kacamata Pria</h2><br>
+                <p>
+                    Menyediakan berbagai jenis kacamata untuk berbagai keperluan khusus untuk pria.
+                    <a href="/menglasses">Tampilkan lebih</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="row"> <!-- 2. Women Glasses -->
+            <img src="./images" alt="">
+            <div class="con-text">
+                <h2>Kacamata Wanita</h2><br><br>
+                <p>
+                    Menyediakan berbagai jenis kacamata untuk berbagai keperluan khusus untuk pria.
+                    <a href="#">Tampilkan lebih</a>
+                </p>
+            </div>
+        </div>          
+    </div>
+
+</section>
+@stop 
+
