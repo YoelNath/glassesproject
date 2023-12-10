447340f7281a5a2ea58697f6e1fb5daa8f45e641
Mintzs <76645203+Zenix101@users.noreply.github.com>
2023-12-11 00:20:33 +0700

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,24 +3,29 @@
 
 <link rel="stylesheet" href="./resources/sass/new.css">
 @include('./components/bsNav')
-<section class="product-categories">
 
-    <div class="products">
-
-        @include('./components/categoryCard')
-        <div class="row"> <!-- 2. Women Glasses -->
-            <img src="./images" alt="">
-            <div class="con-text">
-                <h2>Kacamata Wanita</h2><br><br>
-                <p>
-                    Menyediakan berbagai jenis kacamata untuk berbagai keperluan khusus untuk pria.
-                    <a href="#">Tampilkan lebih</a>
-                </p>
-            </div>
-        </div>
+<div class="card text-bg-dark">
+    <img src="public\images\latest\terbaru1.jpg"  class="card-img" alt="...">
+    <div class="card-img-overlay">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small>Last updated 3 mins ago</small></p>
     </div>
+  </div>
 
-</section>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ URL('public\images\latest\terbaru1.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="g" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
 <footer>
 @include('./components/footer')
 </footer>
