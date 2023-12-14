c68ffc3845fc201037f795e9dcc8eab01223361c
Andrew Wijaya <33770251+andrewwijaya96@users.noreply.github.com>
2023-12-14 19:12:54 +0700

    Edit Navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,61 +4,65 @@
 <link rel="stylesheet" href="./Glassespedia/home.css">
 
 @include('./components/bsNav')
-
-
-  <div class="row-cols-1" >
-
-
-
-
+<div class="row-cols-1">
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="./images/home/zeiss_mini-style-guide.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Trendy Glasses</h5>
-            <p>"Glasses are not just a window to clearer vision; they are a frame for the world's beauty, a reflection of your unique style, and a reminder that sometimes, a different perspective is all we need to see the extraordinary in the ordinary."</p>
-          </div>
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
         </div>
-        <div class="carousel-item">
-          <img src="./images/home/zeiss_mini-style-guide.jpg"class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-            <h5>Stylish Glasses</h5>
-            <p>"Glasses are not just a lens to see the world; they are a frame to envision your unique perspective, a statement of style, and a reflection of the clarity you seek in every moment of life."</p>
-          </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./images/home/zeiss_mini-style-guide.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Trendy Glasses</h5>
+                    <p>"Glasses are not just a window to clearer vision; they are a frame for the world's beauty, a
+                        reflection of your unique style, and a reminder that sometimes, a different perspective is all
+                        we need to see the extraordinary in the ordinary."</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./images/home/zeiss_mini-style-guide.jpg"class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Stylish Glasses</h5>
+                    <p>"Glasses are not just a lens to see the world; they are a frame to envision your unique
+                        perspective, a statement of style, and a reflection of the clarity you seek in every moment of
+                        life."</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="./images/home/zeiss_mini-style-guide.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Chill Glasses</h5>
+                    <p>"Glasses not only frame the world we see, but they also reveal the beauty in every detail,
+                        turning vision into an art, and style into a statement."</p>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
-          <img src="./images/home/zeiss_mini-style-guide.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Chill Glasses</h5>
-            <p>"Glasses not only frame the world we see, but they also reveal the beauty in every detail, turning vision into an art, and style into a statement."</p>
-          </div>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 
     <div class="card text-center">
 
         <div class="card-body">
-      
-          <h5 class="card-title">Trending Today</h5>
-          <p class="card-text">Check out our latest product today</p>
-          <a href="#" class="btn btn-primary">Chill Glasses</a>
+
+            <h5 class="card-title">Trending Today</h5>
+            <p class="card-text">Check out our latest product today</p>
+            <a href="#" class="btn btn-primary">Chill Glasses</a>
         </div>
-      
+
     </div>
 
     <!-- Iklan -->
@@ -70,7 +74,7 @@
         <div class="most-popular">
             <div class="row"> <!-- 1 -->
                 <a href="/oneitem"><img src="./images/popular/palingcari1.jpeg" alt=""></a>
-                
+
                 <div class="product-text">
                     <h5>TERBAIK</h5>
                 </div>
@@ -110,7 +114,7 @@
 
             <div class="row"> <!-- 3 -->
                 <img src="./images/popular/palingcari3.jpg" alt="">
-                
+
                 <div class="product-text">
                     <h5>TERBAIK</h5>
                 </div>
@@ -125,7 +129,8 @@
                     <i class="bx bx-star"></i>
                 </div>
                 <div class="price">
-                    <h4>KACAMATA LENTUR (Anti Radiasi Photocromic Blueray Bluecromic) 2126 / bisa minus dan silinder</h4>
+                    <h4>KACAMATA LENTUR (Anti Radiasi Photocromic Blueray Bluecromic) 2126 / bisa minus dan silinder
+                    </h4>
                     <h3>Rp 140.000</h3>
                 </div>
             </div>
@@ -154,7 +159,8 @@
                     <i class="bx bx-star"></i>
                 </div>
                 <div class="price">
-                    <h4>PL-H019 Kacamata Steampunk Hitam Bersepeda Cyberpunk Hip Hop Punk Gaya Retro Eropa Olahraga Wanita
+                    <h4>PL-H019 Kacamata Steampunk Hitam Bersepeda Cyberpunk Hip Hop Punk Gaya Retro Eropa Olahraga
+                        Wanita
                         Dan Pria</h4>
                     <h3>Rp 80.000</h3>
                 </div>
@@ -162,7 +168,7 @@
 
             <div class="row"> <!-- 2 -->
                 <img src="./images/latest/terbaru2.jpg" alt="">
-        
+
                 <div class="product-text">
                     <h5>Baru</h5>
                 </div>
@@ -184,7 +190,7 @@
 
             <div class="row"> <!-- 3 -->
                 <img src="./images/latest/terbaru3.jpg" alt="">
-                
+
                 <div class="product-text">
                     <h5>Baru</h5>
                 </div>
@@ -203,15 +209,5 @@
                     <h3>Rp 837.000</h3>
                 </div>
             </div>
-
         </div>
-
-
-  {{-- </div>
-
-
-
-  </div> --}}
-
-@include('./components/footer')
-{{-- @stop  --}}
+        @include('./components/footer')
